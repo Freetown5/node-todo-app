ac2102bdccf9c7f48f9262934113c50bbdfc6164
Isa Conteh <iconteh21@gmail.com>
2020-09-15 21:04:20 -0400

    updated template for data, attempted to update pos

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var express = require('express');
-
 var app = express();
 
-app.get('/', function (req, res) {
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.get('/', function (req, res) {
     // res.send('Hello World!');
-    res.render('index');
-});
+    // res.render('index');
+// });
 
 app.listen(3000, function(){
     console.log('Example app listening on port 3000!');
@@ -13,6 +15,16 @@ app.listen(3000, function(){
 
 app.set('view engine', 'ejs');
 
+var task = ["buy socks", "practice with nodejs"];
+
 app.post('/addtask', function(req, res) {
-    res.render('index');
+    var newTask = req.body.newtask;
+    task.push(newTask);
+    res.redirect("/");
+});
+
+// task(array);
+
+app.get("/", function(req, res){
+    res.render("index", { task: task, complete });
 });
