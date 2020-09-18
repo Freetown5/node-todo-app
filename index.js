b5d2d7719a44d4a0cdcc02d61733893f74d83072
Isa Conteh <iconteh21@gmail.com>
2020-09-17 22:01:52 -0400

    added a post request for the task completion

--- index.js
+++ index.js
@@ -4,11 +4,6 @@ var app = express();
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.get('/', function (req, res) {
-    // res.send('Hello World!');
-    // res.render('index');
-// });
-
 app.listen(3000, function(){
     console.log('Example app listening on port 3000!');
 });
@@ -23,8 +18,24 @@ app.post('/addtask', function(req, res) {
     res.redirect("/");
 });
 
-// task(array);
 
 app.get("/", function(req, res){
-    res.render("index", { task: task, complete });
+    res.render("index", {task: task});
+});
+
+var complete = ["finish jquery"];
+
+app.post("/removetask", function(req, res){
+    var completeTask = req.body.check;
+
+    if(typeof completeTask === "string"){
+        complete.push(completeTask);
+        task.splice(task.indexOf(completeTask), 1);
+    }else if(typeof completeTask === "object"){
+        for(var i = 0; i < completeTask.length; i++){
+            complete.push(completeTask[i]);
+            task.splice(task.indexOf(completeTask[i]), 1);
+        }
+    }
+    res.redirect("/");
 });
